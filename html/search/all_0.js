109cc44fe31626e587d0e343a866873b01a3acc9
jsofii <sofi-0106@hotmail.com>
2018-12-18 18:49:00 -0500

    first commit

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['administrador',['Administrador',['../class_administrador.html',1,'']]]
+];
---
+++ html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['conexion',['Conexion',['../class_conexion.html',1,'']]]
+];
